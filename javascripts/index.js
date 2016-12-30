340f5f07bf13c4205471d7632df5af18c6234ef4
dlewis2017 <dlewfaith@gmail.com>
2016-12-29 23:34:50 -0500

    div layout index.html

---
+++ javascripts/index.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCH_4kPXSGIeWunJCznFLDb4fP5PZpSRr4",
+    authDomain: "mentorfunding.firebaseapp.com",
+    databaseURL: "https://mentorfunding.firebaseio.com",
+    storageBucket: "mentorfunding.appspot.com",
+    messagingSenderId: "448935507675"
+};
+
+firebase.initializeApp(config);
+
