f4af27a43e50b61ba3622d2806c57d228c074c36
dlewis2017 <dlewfaith@gmail.com>
2017-02-02 00:33:45 -0500

    starting user profile page and gave ability to logout

---
+++ javascripts/userProfile.js
@@ -0,0 +1,34 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCH_4kPXSGIeWunJCznFLDb4fP5PZpSRr4",
+  authDomain: "mentorfunding.firebaseapp.com",
+  databaseURL: "https://mentorfunding.firebaseio.com",
+  storageBucket: "mentorfunding.appspot.com",
+  messagingSenderId: "448935507675"
+};
+
+firebase.initializeApp(config);
+
+window.onload = function(){
+  //get name to display at top instead of sign in
+  firebase.auth().onAuthStateChanged( function(user) {
+    if (user) {
+      var name = user.email;
+      var display_name = name.split('@')[0];
+      var html_name = document.getElementById('signIn');
+      html_name.innerHTML = display_name;
+      //show user name near login (get inner HTML of that div
+      document.getElementById('signIn').addEventListener('click', logout, false);
+    }
+  });
+}
+
+function logout(){
+  firebase.auth().signOut().then(function(){
+      window.location = "../index.html";
+  }).catch(function(error){
+      console.log("error occured")
+      var errorCode = error.code;
+      alert(errroCode); 
+  });
+}
