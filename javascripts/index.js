d6fc89c185b9d1204befcb4e2b891e8e6933f8cd
dlewis2017 <dlewfaith@gmail.com>
2017-02-02 00:22:09 -0500

    final details on index html and added login capability

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,11 +1,31 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCH_4kPXSGIeWunJCznFLDb4fP5PZpSRr4",
-    authDomain: "mentorfunding.firebaseapp.com",
-    databaseURL: "https://mentorfunding.firebaseio.com",
-    storageBucket: "mentorfunding.appspot.com",
-    messagingSenderId: "448935507675"
+  apiKey: "AIzaSyCH_4kPXSGIeWunJCznFLDb4fP5PZpSRr4",
+  authDomain: "mentorfunding.firebaseapp.com",
+  databaseURL: "https://mentorfunding.firebaseio.com",
+  storageBucket: "mentorfunding.appspot.com",
+  messagingSenderId: "448935507675"
 };
 
 firebase.initializeApp(config);
+
+window.onload = function(){
+  //get name to display at top instead of sign in
+  firebase.auth().onAuthStateChanged( function(user) {
+    if (user) {
+      //show user name near login (get inner HTML of that div
+      document.getElementById('signIn').addEventListener('click', userPage, false);
+    } else {
+      document.getElementById('signIn').addEventListener('click', signIn, false);
+    }
+  });
+}
+
+function signIn(){
+  window.location = "./views/signIn.html";
+}
+
+function userPage(){
+  window.location = "./views/userProfile.html";
+} 
 
---
+++ javascripts/signIn.js
@@ -0,0 +1,42 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCH_4kPXSGIeWunJCznFLDb4fP5PZpSRr4",
+  authDomain: "mentorfunding.firebaseapp.com",
+  databaseURL: "https://mentorfunding.firebaseio.com",
+  storageBucket: "mentorfunding.appspot.com",
+  messagingSenderId: "448935507675"
+};
+
+firebase.initializeApp(config);
+
+window.onload = function(){
+  document.getElementById('signIn').addEventListener('click', signIn, false);
+}
+
+//sing user in with firebase
+function signIn() {
+    var email = document.getElementById('emailTxt').value;
+    var password = document.getElementById('passwordTxt').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Sign in with email and pass.
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
+      window.location = "../index.html";
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error); 
+    }); 
+}
