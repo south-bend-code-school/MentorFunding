17baabb298af0e51d237e6dec15a531e854672de
dlewis2017 <dlewfaith@gmail.com>
2017-02-12 17:03:46 -0500

    aplpy

---
+++ javascripts/apply.js
@@ -0,0 +1,23 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCH_4kPXSGIeWunJCznFLDb4fP5PZpSRr4",
+  authDomain: "mentorfunding.firebaseapp.com",
+  databaseURL: "https://mentorfunding.firebaseio.com",
+  storageBucket: "mentorfunding.appspot.com",
+  messagingSenderId: "448935507675"
+};
+
+firebase.initializeApp(config);
+//global variables
+
+window.onload = function(){
+  //get name to display at top instead of sign in
+  var user = firebase.auth().currentUser;
+  var userId = user.uid;
+  firebase.database().ref('/Users/' + userId+'/personalInfo/').once('value').then(function(snapshot) {
+    var username = Object(snapshot.val()).firstName;
+    user.updateProfile({displayName: username}).catch(function(error){console.log(error)}); 
+    var html_name = document.getElementById('signIn');
+    html_name.innerHTML = user.displayName;
+  });
+}
